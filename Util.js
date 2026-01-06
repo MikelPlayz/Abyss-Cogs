1b1b3f58dc3d5c761af6fc1deeefab3b0626d484
MikelPlayz <93345539+MikelPlayz@users.noreply.github.com>
2026-01-06 12:57:04 -0500

    Marks A New Era.

--- Util.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Util = void 0;
-class Util {
-    static random(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-}
-exports.Util = Util;
